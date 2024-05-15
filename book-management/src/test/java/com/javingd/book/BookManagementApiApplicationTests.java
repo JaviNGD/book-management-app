bc18e19fadf9f866c554bf999d2aaebff5e911fb
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-05-15 15:18:44 -0400

    first commit

---
+++ book-management/src/test/java/com/javingd/book/BookManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javingd.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
