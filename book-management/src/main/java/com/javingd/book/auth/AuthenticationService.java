8bbac6e4291f1408eabd743f0d7bb99ade00a472
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-06-16 21:32:51 -0400

    Create authentication controller and implement register method

---
+++ book-management/src/main/java/com/javingd/book/auth/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.javingd.book.auth;
+
+import com.javingd.book.role.RoleRepository;
+import com.javingd.book.user.User;
+import com.javingd.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+    }
+}
