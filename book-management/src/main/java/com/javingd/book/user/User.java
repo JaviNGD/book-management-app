a398b49a624eeb3d1f5d62320c3723666e859409
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-06-21 22:16:50 -0400

    Implement Book, BookTransactionHistory and Feedback entities and Add Application Audit Aware auditing

--- book-management/src/main/java/com/javingd/book/user/User.java
+++ book-management/src/main/java/com/javingd/book/user/User.java
@@ -1,5 +1,7 @@
 package com.javingd.book.user;
 
+import com.javingd.book.book.Book;
+import com.javingd.book.history.BookTransactionHistory;
 import com.javingd.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -25,7 +27,6 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "_user")
 @EntityListeners(AuditingEntityListener.class)
-
 public class User implements UserDetails, Principal {
 
     @Id
@@ -42,6 +43,12 @@ public class User implements UserDetails, Principal {
 
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
